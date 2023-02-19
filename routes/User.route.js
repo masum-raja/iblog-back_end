6d2f625902517545d644c2dae885f2c14c467c8f
masum <mr.razamasum@gmail.com>
2023-02-19 09:27:09 +0530

    added toolkit

--- routes/User.route.js
+++ routes/User.route.js
@@ -19,7 +19,7 @@ UserRouter.post("/register", async (req, res) => {
       await user_to_add.save((err) => {
         // CHECKING IS EMAIL UNIQUE
         if (err) {
-          res.status(400).send({ error: err.message });
+          res.status(400).send({ message: "Email already exists" });
         } else {
           res.send({ message: "Account created" });
         }
@@ -61,6 +61,15 @@ UserRouter.post("/login", async (req, res) => {
   } catch (error) {
     res.status(400).send({ message: "Something went wrong" });
     console.log(error);
+  }
+});
+
+// LOGIN
+UserRouter.post("/logout", async (req, res) => {
+  try {
+    res.send({ message: "Logged Out" });
+  } catch (error) {
+    res.send({ error: error });
   }
 });
 
