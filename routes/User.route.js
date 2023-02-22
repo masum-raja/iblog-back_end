6ef033363f67c0cf8ab09ab1d73111a052864cbd
alih6051 <alih6051@gmail.com>
2023-02-23 03:14:44 +0530

    save-post implemented

--- routes/User.route.js
+++ routes/User.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const { UserModel } = require("../models/User.model");
 var jwt = require("jsonwebtoken");
+const { authenticateToken } = require("../middleware/authenticateToken");
 
 const UserRouter = express.Router();
 
@@ -44,12 +45,12 @@ UserRouter.post("/login", async (req, res) => {
             { userID: user[0]._id },
             process.env.JWT_SECRET_KEY
           );
-
           res.send({
             name: user[0].name,
             avatar_url: user[0].avatar_url,
             email: user[0].email,
             token: token,
+            saved_posts: user[0].saved_posts,
           });
         } else {
           res.status(400).send({ message: "Wrong Credntials" });
@@ -69,8 +70,42 @@ UserRouter.post("/logout", async (req, res) => {
   try {
     res.send({ message: "Logged Out" });
   } catch (error) {
-    res.send({ error: error });
+    res.status(400).send({ error: error });
   }
 });
+
+// ADD POST TO SAVED
+UserRouter.patch("/saved/add", authenticateToken, async (req, res) => {
+  const userID = req.body.author;
+  const postID = req.body.post_id;
+  try {
+    let doc = await UserModel.findByIdAndUpdate(
+      { _id: userID },
+      { $push: { saved_posts: postID } },
+      { new: true }
+    );
+    res.send(doc.saved_posts);
+  } catch (error) {
+    res.status(400).send({ error: error });
+  }
+});
+
+// REMOVE POST FROM SAVED
+UserRouter.patch("/saved/remove", authenticateToken, async (req, res) => {
+  const userID = req.body.author;
+  const postID = req.body.post_id;
+  try {
+    let doc = await UserModel.findByIdAndUpdate(
+      { _id: userID },
+      { $pull: { saved_posts: postID } },
+      { new: true }
+    );
+    res.send(doc.saved_posts);
+  } catch (error) {
+    res.status(400).send({ error: error });
+  }
+});
+
+UserRouter.get("/", async (req, res) => {});
 
 module.exports = { UserRouter };
