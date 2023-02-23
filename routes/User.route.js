0dcb1e529535ea4cb49c37f99ad9a5c6485a81af
masum <mr.razamasum@gmail.com>
2023-02-23 13:10:50 +0530

    added profile Image

--- routes/User.route.js
+++ routes/User.route.js
@@ -21,7 +21,7 @@ cloudinary.config({
 
  const storage = multer.diskStorage({
    destination: (req, res, cb) => {
-     cb(null, "./tmp");
+     cb(null, "/tmp");
    },
    filename: (req, file, cb) => {
     //  console.log(file);
